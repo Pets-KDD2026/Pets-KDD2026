d0e4d1b3b415147b0efbae8f4fea60f26431a14b
sa13d3asda <3058465675@qq.com>
2025-05-15 17:32:30 +0800

    Add files via upload

---
+++ scripts/zero_shot/PatchTST_Adapter/ETTh1_32.sh
@@ -0,0 +1,111 @@
+model_name=PatchTST_Adapter
+seq_len=336
+d_model=32		# 64
+d_ff=64			# 128
+n_heads=4		# 8
+e_layers=4
+batch_size=64
+
+python -u run_for_ZeroShot.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_$seq_len\_96 \
+  --model $model_name \
+  --seq_len $seq_len \
+  --e_layers $e_layers \
+  --n_heads $n_heads \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --batch_size $batch_size \
+  --data ETTh1 \
+  --features M \
+  --label_len 0 \
+  --pred_len 96 \
+  --d_layers 0 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --learning_rate 0.0005 \
+  --itr 1
+
+python -u run_for_ZeroShot.py \
+   --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_$seq_len\_192 \
+  --model $model_name \
+  --seq_len $seq_len \
+  --e_layers $e_layers \
+  --n_heads $n_heads \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --batch_size $batch_size \
+  --data ETTh1 \
+  --features M \
+  --label_len 0 \
+  --pred_len 192 \
+  --d_layers 0 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --learning_rate 0.0005 \
+  --itr 1
+
+python -u run_for_ZeroShot.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_$seq_len\_336 \
+  --model $model_name \
+  --seq_len $seq_len \
+  --e_layers $e_layers \
+  --n_heads $n_heads \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --batch_size $batch_size \
+  --data ETTh1 \
+  --features M \
+  --label_len 0 \
+  --pred_len 336 \
+  --d_layers 0 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --learning_rate 0.0005 \
+  --itr 1
+
+python -u run_for_ZeroShot.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_$seq_len\_720 \
+  --model $model_name \
+  --seq_len $seq_len \
+  --e_layers $e_layers \
+  --n_heads $n_heads \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --batch_size $batch_size \
+  --data ETTh1 \
+  --features M \
+  --label_len 0 \
+  --pred_len 720 \
+  --d_layers 0 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --learning_rate 0.0005 \
+  --itr 1
---
+++ scripts/zero_shot/PatchTST_Adapter/ETTm2_16.sh
@@ -0,0 +1,111 @@
+model_name=PatchTST_Adapter
+seq_len=336
+d_model=16		# 64
+d_ff=32			# 128
+n_heads=2		# 8
+e_layers=4
+batch_size=64
+
+python -u run_for_ZeroShot.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm2.csv \
+  --model_id ETTm2_$seq_len\_96 \
+  --model $model_name \
+  --seq_len $seq_len \
+  --e_layers $e_layers \
+  --n_heads $n_heads \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --batch_size $batch_size \
+  --data ETTm2 \
+  --features M \
+  --label_len 0 \
+  --pred_len 96 \
+  --d_layers 0 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --learning_rate 0.0005 \
+  --itr 1
+
+python -u run_for_ZeroShot.py \
+   --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm2.csv \
+  --model_id ETTm2_$seq_len\_192 \
+  --model $model_name \
+  --seq_len $seq_len \
+  --e_layers $e_layers \
+  --n_heads $n_heads \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --batch_size $batch_size \
+  --data ETTm2 \
+  --features M \
+  --label_len 0 \
+  --pred_len 192 \
+  --d_layers 0 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --learning_rate 0.0005 \
+  --itr 1
+
+python -u run_for_ZeroShot.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm2.csv \
+  --model_id ETTm2_$seq_len\_336 \
+  --model $model_name \
+  --seq_len $seq_len \
+  --e_layers $e_layers \
+  --n_heads $n_heads \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --batch_size $batch_size \
+  --data ETTm2 \
+  --features M \
+  --label_len 0 \
+  --pred_len 336 \
+  --d_layers 0 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --learning_rate 0.0005 \
+  --itr 1
+
+python -u run_for_ZeroShot.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm2.csv \
+  --model_id ETTm2_$seq_len\_720 \
+  --model $model_name \
+  --seq_len $seq_len \
+  --e_layers $e_layers \
+  --n_heads 4 \
+  --d_model 32 \
+  --d_ff 64 \
+  --batch_size $batch_size \
+  --data ETTm2 \
+  --features M \
+  --label_len 0 \
+  --pred_len 720 \
+  --d_layers 0 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --learning_rate 0.0005 \
+  --itr 1
