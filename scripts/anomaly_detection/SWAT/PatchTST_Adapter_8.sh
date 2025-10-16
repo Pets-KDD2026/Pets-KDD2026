1c53df752945e11b658aaf165b475584f0a91abb
Pets <3058465675@qq.com>
2025-10-16 23:09:26 +0800

    Add files via upload

---
+++ scripts/anomaly_detection/SWAT/PatchTST_Adapter_8.sh
@@ -0,0 +1,29 @@
+model_name=PatchTST_Adapter
+d_model=8
+d_ff=16
+n_heads=2
+e_layers=4
+batch_size=128
+
+python -u run.py \
+  --task_name anomaly_detection \
+  --is_training 1 \
+  --root_path ./dataset/SWaT \
+  --model_id SWAT \
+  --data SWAT \
+  --model $model_name \
+  --e_layers $e_layers \
+  --n_heads $n_heads \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --batch_size $batch_size \
+  --features M \
+  --seq_len 100 \
+  --pred_len 0 \
+  --factor 3 \
+  --enc_in 51 \
+  --c_out 51 \
+  --anomaly_ratio 1 \
+  --learning_rate 0.00005 \
+  --train_epochs 40 \
+  --itr 1
